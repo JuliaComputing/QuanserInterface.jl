b238f692c76a4750c85527ca59687c9987ba5855
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2023-07-10 12:51:04 +0200

    update swingup and balance demo

--- test/runtests.jl
+++ test/runtests.jl
@@ -30,16 +30,14 @@ include("../examples/furuta_lqg.jl")
 ##
 velfilt1 = c2d(ss(tf([50, 0], [1, 50])), Ts, :tustin) |> SysFilter
 velfilt2 = c2d(ss(tf([50, 0], [1, 50])), Ts, :tustin) |> SysFilter
-gmffilt = SysFilter(Cgmf)
+# gmffilt = SysFilter(Cgmf)
 #
 
 using QuanserInterface: energy
 measure = QuanserInterface.measure
 
-function controller(u, y, obsfilter)
+function controller(u, y, θ̇, obsfilter)
     θ = y[2] - pi
-    ϕ̇ = velfilt1(y[1])[]
-    θ̇ = velfilt2(θ)[]
     θ = normalize_angles_pi(θ)
     xh = obsfilter([u; y[1]; θ])
     # ugmf = gmffilt([y[1]; θ])
@@ -50,52 +48,77 @@ function controller(u, y, obsfilter)
         # u =- ugmf
         u, xh
     else
-        u = 0*[80*(E - energy(0,0))*sign(θ̇*cos(θ)) - 0.1*y[1]] # Bias slightly towards center
-        @. u = clamp(u, -4, 4)
+        u = [80*(E - energy(0,0))*sign(θ̇*cos(θ)) - 0.2*y[1]] # Bias slightly towards center
+        @. u = clamp(u, -10, 10)
     end
     u, xh, E
 end
 
 
-function balance_demo(p; 
+function swingup_balance_demo(p; 
     u_max = 2.0,
     Tf = 10,
     controller = controller,
     obsfilter = SysFilter(obs, copy(x0)),
 )
 
-    velfilt1.state .= 0
-    velfilt2.state .= 0
-    # initialize(p)
     Ts = p.Ts
     N = round(Int, Tf/Ts)
     data = Vector{Vector{Float64}}(undef, 0)
     sizehint!(data, N)
+        
+    simulation = processtype(process) isa SimulatedProcess
     
+    if simulation
+        u0 = 0.0
+    else
+        u0 = 0.5QuanserInterface.go_home(process, r = 0, K = 0.05, Ki=0.2, Kf=0.02)
+        @show u0
+    end
+    y = QuanserInterface.measure(process)
+    if !simulation
+        @info "Starting $(simulation ? "simulation" : "experiment") from y: $y, waiting for your input..."
+        readline()
+    end
+
+    yo = zeros(2)
+    dyf = zeros(2)
     t_start = time()
-    # y_start .* [1, 1]
-    simulation = processtype(p) isa SimulatedProcess
-    simulation || QuanserInterface.go_home(p; K = 0.2, Ki=0.5, Kf=0.0)
-    y_start = measure(p)
     try
         GC.gc()
         GC.enable(false)
         u = [0.0]
+        oob = 0
         for i = 1:N
             @periodically_yielding Ts begin 
                 t = time() - t_start
-                y = measure(p) - y_start # Subtract initial position for a smoother experience
-                u, xh, E = controller(u, y, obsfilter)
+                y = measure(p)
+                dy = (y - yo) ./ Ts
+                @. dyf = 0.5dyf + 0.5dy
+
+                u, xh, E = controller(u, y, dyf[2], obsfilter)
                 u = @. clamp(u, -u_max, u_max)
+                if !(-deg2rad(110) <= y[1] <= deg2rad(110))
+                    u = [-0.5*y[1]]
+                    @warn "Correcting"
+                    oob += 20
+                    if oob > 600
+                        @error "Out of bounds"
+                        break
+                    end
+                else
+                    oob = max(0, oob-1)
+                end
                 @show u
                 control(p, Vector(u))
                 log = [t; y; xh; u; E]
                 push!(data, log)
+                yo = y
             end
         end
     catch e
         @error "Shit hit the fan" e
-        rethrow()
+        # rethrow()
     finally
         control(p, [0.0])
         GC.enable(true)
@@ -107,5 +130,10 @@ end
 
 ##
 
-D = balance_demo(process; u_max=8, Tf = 20)
+D = swingup_balance_demo(process; u_max=10, Tf = 12)
 plotD(D)
+
+##
+using DelimitedFiles
+cd(@__DIR__)
+# writedlm("swingup.csv", [["t" "angle_arm" "angle_pend" "control_input"]; D[[1,2,3,8], :]'], ',')
