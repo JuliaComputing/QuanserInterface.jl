e7bb3c7eb3d63a2db5e1fdd8551c4f1a833f2737
Anshul Singhvi <anshulsinghvi@gmail.com>
2025-07-29 17:12:23 -0400

    Clean up the swingup_power_control example and use WGLMakie

--- examples/interactive/swingup_power_control_basic.jl
+++ examples/interactive/swingup_power_control_basic.jl
@@ -13,52 +13,75 @@ using HardwareAbstractions
 using ControlSystemsBase
 using QuanserInterface: energy, measure
 using StaticArrays
-import Plots
-using GLMakie
+using WGLMakie
 
+
+# Define some useful parameters and constants 
+# to control the Quanser pendulum
 const rr = Ref([0, pi, 0, 0])
 nu  = 1     # number of controls
 nx  = 4     # number of states
 Ts  = 0.005 # sampling time
 
-
+# Initialize the Quanser pendulum, your computer
+# must be connected to it at this point.
 process = QuanserInterface.QubeServoPendulum(; Ts)
 rr[][1] = deg2rad(0)
 rr[][2] = pi
+# Run a test measurement, this returns two angles
+# in radians, the first is the angle of the "block"/rotator,
+# the second is the angle of the arm.
 y = QuanserInterface.measure(process)
 
+# Create a figure to visualize the pendulum.
 fig = Figure()
+# The `LScene` is a 3D graphics context on which we will plot the pendulum.
+# This is currently quite primitive, but we can easily beautify it by using a `mesh!` plot
 visualization_scene = LScene(fig[1, 1])
+# The "block" points straight ahead
 block_plot = lines!(visualization_scene, [Point3f(0, 0, 0), Point3f(1, 0, 0)]; linewidth = 5, color = :gray)
+# The "arm" is assumed to be hanging down initially.
 arm_plot = lines!(visualization_scene, [Point3f(1, 0, 0), Point3f(1, 0, -1)]; linewidth = 5, color = :red)
 
+# This slider grid will allow us to control the force applied to the pendulum.
+# You can add more named tuples to create more sliders, that can update more parameters.
 sg = SliderGrid(
     fig[2, 1],
     (; label = "Force", range = LinRange(60, 90, 100), startvalue = 80.0, format = "{:.2f}");
     tellwidth = false,
     tellheight = true
 )
+# Slider observables are of type Any by default
 force_untyped = sg.sliders[1].value
-force = Observable{Float64}(force_untyped[])
-on(force_untyped) do f
-    force[] = f
-end
+# but you can force it to be of a certain type
+force = lift(Float64, force_untyped)
 
-angles = Observable{Tuple{Float64, Float64}}((0.0, 0.0))
-
+# This callback is called every time the figure is rendered.
+# It updates the plots to reflect the current state of the pendulum.
 on(events(fig).tick) do tick
     (block_angle, arm_angle) = QuanserInterface.measure(process)
+    # Rotate the block around the z-axis
     block_rotation = to_rotation((Vec3f(0, 0, 1), block_angle))
+    # Rotate the arm around the x-axis which is its axis of rotation
     arm_rotation   = to_rotation((Vec3f(1, 0, 0), arm_angle))
-
+    # Rotate the plots to reflect the current state of the pendulum.
     rotate!(block_plot, block_rotation)
     rotate!(arm_plot, block_rotation * arm_rotation)
 end
 
+# ## Control code
+# From here, all the code is adapted from the `swingup.jl` example.
+# The main difference is that we use the `@periodically_yielding` macro
+# allows the graphics context to be updated in the main thread,
+# while the control loop runs in the background.
 normalize_angles(x::Number) = mod(x, 2pi)
 normalize_angles(x::AbstractVector) = SA[(x[1]), normalize_angles(x[2]), x[3], x[4]]
 
-function swingup(process; force = Ref(80.0), angles = Ref((0.0, 0.0)), Tf = 10, verbose=true, stab=true, umax=2.0)
+# Note the new kwarg here - the syntax
+# to update a Ref and an Observable is the same
+# so they are interchangeable, and you can pass 
+# either an Observable or a Ref to `force`.
+function swingup(process; force = Ref(80.0), Tf = 10, verbose=true, stab=true, umax=2.0)
     Ts = process.Ts
     N = round(Int, Tf/Ts)
     data = Vector{Vector{Float64}}(undef, 0)
@@ -88,11 +111,11 @@ function swingup(process; force = Ref(80.0), angles = Ref((0.0, 0.0)), Tf = 10, 
         t_start = time()
         u = [0.0]
         oob = 0
+
         for i = 1:N
-            @periodically Ts begin 
+            @periodically_yielding Ts begin 
                 t = simulation ? (i-1)*Ts : time() - t_start
                 y = QuanserInterface.measure(process)
-                angles[] = (y[1], y[2])
                 dy = (y - yo) ./ Ts
                 dyf = @. 0.5dyf + 0.5dy
                 xh = [y; dyf]
@@ -126,7 +149,8 @@ function swingup(process; force = Ref(80.0), angles = Ref((0.0, 0.0)), Tf = 10, 
                         αr = r[2] - pi
                         α̇ = xh[4]
                         E = energy(α, α̇)
-                        uE = force[]*(E - energy(αr,0))*sign(α̇*cos(α))
+                        # NOTE: this is where you get the force
+                        uE = force[] * (E - energy(αr,0))*sign(α̇*cos(α))
                         u = SA[clamp(uE - 0.2*y[1], -umax, umax)]
                     end
                     control(process, Vector(u))
@@ -158,26 +182,10 @@ elseif abs(y[2]) > 0.8 || !(-2.5 < y[1] < 2.5)
     @info "Auto homing"
     autohome!(process)
 end
-D = Threads.@spawn swingup(process; force = force, angles = angles, Tf = 20, verbose = false)
+# Spawn the control loop in a new thread.
+# This allows graphics updates to happen on the main thread,
+# while the control loop runs separately, but in the same process.
+task = Threads.@spawn swingup(process; force = force, Tf = 200, verbose = false)
 
-
-
-# ## Simulated process
-# process = QuanserInterface.QubeServoPendulumSimulator(; Ts, p = QuanserInterface.pendulum_parameters(true));
-
-# @profview_allocs runplot(process; Tf = 5) sample_rate=0.1
-
-# ##
-
-# task = @spawn runplot(process; Tf = 15)
-# rr[][1] = deg2rad(-30)
-# rr[][1] = deg2rad(-20)
-# rr[][1] = deg2rad(-10)
-# rr[][1] = deg2rad(0)
-# rr[][1] = deg2rad(10)
-# rr[][1] = deg2rad(20)
-# rr[][1] = deg2rad(30)
-
-
-# rr[][2] = pi
-# rr[][2] = 0
+# To interrupt:
+# Base.throwto(task, InterruptException())
